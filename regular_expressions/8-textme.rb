27fbc0150ca8a3150e333a4f03de2c8f05872ed2
Sharif2138 <s.kiviiri@alustudent.com>
2024-09-09 05:49:02 -0700

    phone number

--- regular_expressions/8-textme.rb
+++ regular_expressions/8-textme.rb
@@ -1,1 +1,8 @@
+#!/usr/bin/env ruby
 
+log_entry = ARGV[0]
+sender = log_entry[/\[from:(.*?)\]/, 1]
+receiver = log_entry[/\[to:(.*?)\]/, 1]
+flags = log_entry[/\[flags:(.*?)\]/, 1]
+
+puts "#{sender},#{receiver},#{flags}"
